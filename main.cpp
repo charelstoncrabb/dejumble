b0b4251e378a925f3d238879591a671d8667f666
Nicholas Crabb <charcr466@gmail.com>
2019-07-30 22:48:45 -0700

    templated trie implemented, brief test in main.cpp

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <iostream>
 
-#include "src/Trie.hpp"
+#include "src/TriedContainer.hpp"
 #include "extern/argparse/argparse.hpp"
 
 using namespace std;
@@ -32,5 +32,24 @@ int main(int argc, const char** argv)
 {
 	pair<string,size_t> jumbled_prob = parse_args(argc,argv);
 
+	string charsUsing = jumbled_prob.first;
+	size_t minlen = jumbled_prob.second;
+
+	TriedContainer<> trie;
+
+	if (trie.insert(charsUsing.data(),charsUsing.size(),1))
+		cout << "Insert of " << charsUsing << " successful!" << endl;
+
+	int datum = 0;
+	if (trie.find(charsUsing.data(),charsUsing.size(),datum))
+		cout << "Find of " << charsUsing << " successul! Found datum " << datum << endl;
+	
+	if (trie.erase(charsUsing.data(),charsUsing.size()))
+		cout << "Erase of " << charsUsing << " successful!" << endl;
+	
+	cout << "trie.size() = " << trie.size() << endl;
+
+	if (!trie.find(charsUsing.data(),charsUsing.size(),datum))
+		cout << "Not find of " << charsUsing << " successful!" << endl;
 	return 0;
 }
